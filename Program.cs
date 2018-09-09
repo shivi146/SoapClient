ac3046f6c6a5f0dfd27c264776ecf54166e7a061
Shivangi Garg <gargshivangi@gmail.com>
2018-09-09 20:20:32 +0530

    first comit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapInvoker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">>> hello");
+
+            ServiceReference1.Person personDetails = new ServiceReference1.Person();
+            personDetails.Name = "Rishab";
+            personDetails.Age = 30;
+            ServiceReference1.WebServiceCalculatorSoapClient scr = new ServiceReference1.WebServiceCalculatorSoapClient();
+            string str = scr.SavePersonDetails(personDetails);
+            Console.WriteLine(">>> result " + str);
+            Console.ReadKey();
+        }
+    }
+}
